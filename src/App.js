38f3262d09c783a857a762146962ddc1cb457a84
Iryna Yehorova <iryna.yehorova@faceit.com.ua>
2022-05-24 15:57:16 +0300

    add tailwind, axios, template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [colors, setColors] = useState([])
+  const [loading, setLoading] = useState(false)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="flex flex-col md:px-12 px-4 bg-background font-poppins items-center min-h-screen">
+			<h1 className="md:text-6xl text-4xl font-bold text-primary mt-10">
+				Random <span className="text-active">Color Pallete</span>{' '}
+				Generator
+			</h1>
+			<h2 className="text-primary text-2xl font-light mt-6 font-ebas">
+				Click change to get a random color pallete
+			</h2>
+
+      <button className="mt-10 font-bold text-primary text-xl hover:text-active">
+        {
+          loading ? (<span className="animate-pulse">Loading...</span>) : (<>Change &rarr;</>)
+        }
+			</button>
+		</div>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkeMode: false,
+  theme: {
+    colors: {
+      background: "#000",
+      primary: "#FFF",
+      active: "#FFCF00"
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+}
